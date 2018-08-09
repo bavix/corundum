2e728927eb4fad9ae82a19f654d86482e5d6ed7e
Babichev Maxim <babichev@mail.tvil.ru>
2018-08-09 14:01:55 +0300

    update templates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,39 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">
+                <img src="/svg/logo.svg" class="logo" />
+            </a>
+            <button class="navbar-toggler" type="button"
+                    data-toggle="collapse"
+                    data-target="#navbar"
+                    aria-controls="navbar"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('dashboard') }}">Dashboard <span class="sr-only">(current)</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
     <main id="app" class="container">
         <div class="row">
             <div class="col-md-3">
+                <bucket-menu v-bind:items="buckets"></bucket-menu>
+            </div>
+            <div class="col-md-3">
+                <bucket-menu v-bind:items="buckets"></bucket-menu>
+            </div>
+            <div class="col-md-6">
                 <bucket-menu v-bind:items="buckets"></bucket-menu>
             </div>
             {{--@yield('content')--}}
