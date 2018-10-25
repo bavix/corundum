d3d13fc4233a3c485808963db4cba1ed11bda874
Babichev Maxim <babichev@mail.tvil.ru>
2018-10-25 18:40:59 +0300

    add validation

---
+++ app/Http/Rules/ColorRule.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Rules;
+
+use Illuminate\Validation\Rule;
+use Intervention\Image\ImageManagerStatic;
+
+class ColorRule extends Rule
+{
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value): bool
+    {
+        try {
+            ImageManagerStatic::canvas(1, 1, $value);
+        } catch (\Throwable $throwable) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message(): string
+    {
+        return trans('corundum.rules.color');
+    }
+
+}
