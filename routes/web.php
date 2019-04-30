8c34bdc1667cab889c3ad749750e7fdace79c0b5
Babichev Maxim <babichev@mail.tvil.ru>
2019-04-30 13:09:27 +0300

    add route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Auth::routes();
 
 Route::view('/', 'welcome')
@@ -19,3 +21,18 @@ Route::view('/', 'welcome')
 Route::get('/dashboard', 'DashboardController@index')
     ->middleware('auth')
     ->name('dashboard');
+
+Route::get('/{bucket}/{view}/{uuid}.{type}', function (string $bucket, string $view, string $uuid, string $type) {
+    $type = ".$type";
+    if ($type !== '.webp') {
+        $type = '';
+    }
+
+    \header("X-Accel-Redirect: /stream/$bucket/$view/$uuid$type");
+    die;
+})->where([
+    'bucket' => '[a-z0-9]+',
+    'view' => '[a-z0-9]+',
+    'uuid' => '[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}',
+    'type' => '(png|webp)'
+]);
