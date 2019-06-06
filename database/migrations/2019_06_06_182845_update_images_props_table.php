01868b19870e957eb9105cad53e8b12affea012e
Babichev Maxim <babichev@mail.tvil.ru>
2019-06-06 17:30:47 +0300

    add migration's

---
+++ database/migrations/2019_06_06_182845_update_images_props_table.php
@@ -0,0 +1,82 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+
+    /**
+     * @return array
+     */
+    protected function attributes(): array
+    {
+        return [
+            [
+                'type' => 'varchar',
+                'name' => 'MIME type',
+                'slug' => 'mime',
+                'entities' => [\App\Models\Image::class],
+                'group' => 'metadata',
+            ],
+            [
+                'type' => 'integer',
+                'name' => 'Image width',
+                'slug' => 'width',
+                'entities' => [\App\Models\Image::class],
+                'group' => 'metadata',
+            ],
+            [
+                'type' => 'integer',
+                'name' => 'Image height',
+                'slug' => 'height',
+                'entities' => [\App\Models\Image::class],
+                'group' => 'metadata',
+            ],
+            [
+                'type' => 'integer',
+                'name' => 'File size',
+                'slug' => 'size',
+                'entities' => [\App\Models\Image::class],
+                'group' => 'metadata',
+            ],
+            [
+                'type' => 'varchar',
+                'name' => 'Image fingerprint',
+                'slug' => 'fingerprint',
+                'entities' => [\App\Models\Image::class],
+                'group' => 'metadata',
+            ],
+            [
+                'type' => 'boolean',
+                'name' => 'Alpha channel',
+                'slug' => 'alpha',
+                'entities' => [\App\Models\Image::class],
+                'group' => 'metadata',
+            ]
+        ];
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach ($this->attributes() as $attribute) {
+            app('rinvex.attributes.attribute')
+                ->create($attribute);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // todo
+    }
+
+}
